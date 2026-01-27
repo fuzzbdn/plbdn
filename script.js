f2e9c06da40ca19a98a0ee0ad474d23b1131be99
fuzzbdn <magnus.moreskog@gmail.com>
2026-01-27 11:32:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /* =========================================
    1. KONFIGURATION & GLOBALA VARIABLER
-   ========================================= */
+   ======================================== */
 const DEFAULT_STATIONS = [
     { name: "Björkliden", color: "#ffb74d" },
     { name: "Kiruna",     color: "#fff176" },
@@ -631,5 +631,6 @@ async function removeUser(u) {
         renderRoster();
     } 
 }
+
 
 
