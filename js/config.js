654578769e358912830bf6819b98ccf0d7a0dc02
fuzzbdn <magnus.moreskog@gmail.com>
2026-01-29 10:47:17 +0100

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,19 @@
+export const DEFAULT_STATIONS = [
+    { name: "Björkliden", color: "#ffb74d" },
+    { name: "Kiruna",     color: "#fff176" },
+    { name: "Bastuträsk", color: "#e57373" },
+    { name: "Boden",      color: "#81c784" },
+    { name: "Gällivare",  color: "#64b5f6" },
+    { name: "Älvsbyn",    color: "#e0e0e0" },
+    { isSpacer: true },
+    { name: "Info",       color: "#f06292" },
+    { name: "PL",         color: "#0277bd" }
+];
+
+export const DEFAULT_SHIFTS = [
+    { label: "Förmiddag",  time: "06:30 - 14:00" },
+    { label: "Eftermiddag", time: "14:00 - 21:15" },
+    { label: "Natt",        time: "21:15 - 06:30" }
+];
+
+export const DAYS = ["Måndag", "Tisdag", "Onsdag", "Torsdag", "Fredag", "Lördag", "Söndag"];
