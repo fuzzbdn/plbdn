8195c127608e21007257fe51621a7e99714407d1
fuzzbdn <magnus.moreskog@gmail.com>
2026-06-15 15:04:59 +0200

    Update store.js

--- js/store.js
+++ js/store.js
@@ -1,1 +1,10 @@
+// js/store.js
+export const state = {
+    scheduleData: {},
+    users: [],
+    stations: [],
+    shifts: []
+};
 
+export function setUsers(data) { state.users = data; }
+export function getUsers() { return state.users; }
