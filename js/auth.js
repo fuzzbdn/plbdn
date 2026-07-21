b41c27842710de61c6ce20c2c061c3607253b351
fuzzbdn <magnus.moreskog@gmail.com>
2026-07-21 15:16:16 +0200

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -36,7 +36,7 @@ async function applyCustomTheme() {
 }
 
 function isValidEmail(email) {
-    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+    return /^[^\s@]+@[^\s@.]+\.[^\s@]+$/.test(email);
 }
 
 // ==========================================
